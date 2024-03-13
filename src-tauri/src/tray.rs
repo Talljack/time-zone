9fa8c2c2b745a560667a6d554d3b75e3dfe387e5
Yugang Cao <1442770751@qq.com>
2024-03-13 23:44:46 +0800

    feat: add submenu (#1)

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,12 +1,12 @@
 use tauri::{
-    menu::{Menu, MenuItem},
+    menu::{Menu, MenuItem, Submenu},
     tray::{ClickType, TrayIcon},
     Runtime,
 };
 use tauri::Manager;
 use crate::UPDATE_RESULT;
 use chrono::{DateTime, Utc};
-use chrono_tz::Tz;
+use chrono_tz::{Tz, TZ_VARIANTS};
 use crate::windows::{
     show_updater_window, get_settings_window,
     SETTINGS_WIN_NAME
@@ -14,6 +14,7 @@ use crate::windows::{
 use std::thread;
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
+use std::str::FromStr;
 
 pub fn create_tray<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<()> {
     // let config = get_config().unwrap();
@@ -29,7 +30,11 @@ pub fn create_tray<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<()> {
             .set_text("💡 New version available!")
             .unwrap();
     }
-    let setting_i = MenuItem::with_id(app, "settings", "Settings", true, None::<String>)?;
+    let sub_setting_i = Submenu::with_items(app, "Settings", true, &[])?;
+    for item in TZ_VARIANTS {
+        let sub_item = MenuItem::with_id(app, item, item.to_string(), true, None::<String>)?;
+        let _ = sub_setting_i.append(&sub_item);
+    }
     let show_i = MenuItem::with_id(app, "show", "Show", true, None::<String>)?;
     let hide_i = MenuItem::with_id(app, "hide", "Hide", true, None::<String>)?;
     let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<String>)?;
@@ -37,7 +42,7 @@ pub fn create_tray<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<()> {
         app,
         &[
             &check_for_updates_i,
-            &setting_i,
+            &sub_setting_i,
             &show_i,
             &hide_i,
             &quit_i,
@@ -46,27 +51,53 @@ pub fn create_tray<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<()> {
 
     let tray = app.tray().expect("Failed to create tray icon");
     tray.set_menu(Some(menu.clone()))?;
-    tray.on_menu_event(move |app, event| match event.id.as_ref() {
-        "check_for_updates" => {
+    // tray.on_menu_event(move |app, event| match event.id.as_ref() {
+    //     "check_for_updates" => {
+    //         show_updater_window();
+    //     }
+    //     // TZ_VARIANTS[0] => {
+    //     //     // get_settings_window();
+    //     //     println!("click")
+    //     // }
+    //     TZ_VARIANTS => {
+
+    //     }
+    //     "show" => {
+    //         let window = app.get_webview_window(SETTINGS_WIN_NAME).unwrap();
+    //         window.set_focus().unwrap();
+    //         window.unminimize().unwrap();
+    //         window.show().unwrap();
+    //     }
+    //     "hide" => {
+    //         let window = app.get_webview_window(SETTINGS_WIN_NAME).unwrap();
+    //         window.set_focus().unwrap();
+    //         window.unminimize().unwrap();
+    //         window.hide().unwrap();
+    //     }
+    //     "quit" => app.exit(0),
+    //     _ => {}
+    // });
+    tray.on_menu_event(move|app, event| {
+        // println!("event: {}", event.id.as_ref());
+        let id_str = event.id.as_ref();
+        let id_tz = Tz::from_str(&id_str).unwrap();
+        if id_str == "check_for_updates" {
             show_updater_window();
-        }
-        "settings" => {
-            get_settings_window();
-        }
-        "show" => {
+        } else if id_str == "show" {
             let window = app.get_webview_window(SETTINGS_WIN_NAME).unwrap();
             window.set_focus().unwrap();
             window.unminimize().unwrap();
             window.show().unwrap();
-        }
-        "hide" => {
+        } else if id_str == "hide" {
             let window = app.get_webview_window(SETTINGS_WIN_NAME).unwrap();
             window.set_focus().unwrap();
             window.unminimize().unwrap();
             window.hide().unwrap();
+        } else if id_str == "quit" {
+            app.exit(0);
+        } else if TZ_VARIANTS.contains(&id_tz) {
+            println!("TZ: {}", id_str);
         }
-        "quit" => app.exit(0),
-        _ => {}
     });
     tray.on_tray_icon_event(|tray, event| {
         if event.click_type == ClickType::Left {
@@ -81,7 +112,6 @@ pub fn create_tray<R: Runtime>(app: &tauri::AppHandle<R>) -> tauri::Result<()> {
     let tray = TrayIcon::clone(&tray);
     let shared_tray = Arc::new(Mutex::new(tray.clone()));
     tray_update::<R>(shared_tray);
-    // let _ = tray.set_title(Some(tray_time.format("%d/%m/%Y %H:%M").to_string()));
     let _ = tray.set_icon_as_template(false);
     tray.set_show_menu_on_left_click(false)?;
 
@@ -97,7 +127,7 @@ fn tray_update<R: Runtime>(tray: Arc<Mutex<TrayIcon<R>>>) {
             let format_time = new_york_time.format("%m-%d %H:%M");
 
             // Lock the tray icon and update the title
-            let mut locked_tray = tray.lock().unwrap();
+            let locked_tray = tray.lock().unwrap();
             let _ = locked_tray.set_title(Some(format_time.to_string()));
 
             // Sleep for 60 seconds
